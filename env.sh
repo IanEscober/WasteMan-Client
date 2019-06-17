37863f3ee2e3fa02c1f2a1168b13dce8e1c82105
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2019-06-17 23:02:27 +0800

    Refactored the env script to be modular and readable

--- env.sh
+++ env.sh
@@ -1,38 +1,60 @@
 #!/bin/sh
 
-rm -rf ./env.js
-touch ./env.js
+readonly ENV_FORMAT="window._env_"
+readonly REACT_ENV_FILTER="REACT_APP_"
+readonly REGEX_KEY="s/=.*//"
+readonly REGEX_VALUE="s/^[^=]*=//"
+readonly INPUT_FILE=./.env
+readonly OUTPUT_FILE=./env.js
 
-echo "window._env_ = {" >> ./env.js
+isReactEnv() {
+  ( [ "${1%"${1#??????????}"}" = $REACT_ENV_FILTER ] && true ) || false
+}
 
-if [ -f ./.env ]; then
+isKeyValuePair() {
+  ( printf "%s\n" "$1" | grep -q -e '=' && true ) || false
+}
+
+getKey() {
+  isKeyValuePair "$1" && printf "%s\n" "$1" | sed -e "$REGEX_KEY"
+}
+
+getValue() {
+  isKeyValuePair "$1" && printf "%s\n" "$1" | sed -e "$REGEX_VALUE"
+}
+
+toJson() {
+  echo "  $1: \"$2\"," >> $OUTPUT_FILE
+}
+
+ReadFromFile() {
   while read -r line || [ -n "$line" ];
   do
-    if printf '%s\n' "$line" | grep -q -e '='; then
-      key=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-      tempValue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
-    fi
-
-    if [ "${key%"${key#??????????}"}" = "REACT_APP_" ]; then
+    isReactEnv "$line" && ( 
+      key=$( getKey "$line" )
+      tempValue=$( getValue "$line")
       eval value="\$$key"
       [ -z "$value" ] && value=${tempValue}
-      
-      echo "  $key: \"$value\"," >> ./env.js
-    fi
-  done < ./.env
-else
+      toJson "$key" "$value"
+    )
+  done < $INPUT_FILE
+}
+
+ReadFromEnv() {
   printenv | \
   while read -r line || [ -n "$line" ];
   do
-    if printf '%s\n' "$line" | grep -q -e '='; then
-      key=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-      value=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
-    fi
-    
-    if [ "${key%"${key#??????????}"}" = "REACT_APP_" ]; then
-      echo "  $key: \"$value\"," >> ./env.js
-    fi
+    isReactEnv "$line" && (
+      key=$( getKey "$line" )
+      value=$( getValue "$line")
+      toJson "$key" "$value"
+    )
   done
-fi
+}
 
-echo "}" >> ./env.js
+rm -rf $OUTPUT_FILE
+touch $OUTPUT_FILE
+
+echo "$ENV_FORMAT = {" >> $OUTPUT_FILE
+( [ -f $INPUT_FILE ] && ReadFromFile ) || ReadFromEnv
+echo "}" >> $OUTPUT_FILE
